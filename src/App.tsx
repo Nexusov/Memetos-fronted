3ede804d8bdbfacf45c5830f45e666baba56dfc2
Nexus <egor2001grashin@gmail.com>
2023-04-28 01:57:49 +0300

    show Authorized Page in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Home from './pages/Home'
+import Invitation from './pages/Invitation'
+import CodeInput from './pages/CodeInput'
+
 import style from './App.module.scss'
+import AuthorizedPage from './pages/AuthorizedPage'
 
 const App = () => {
   return (
     <div className={style.container}>
-      
+      {/* <Home /> */}
+      {/* <Invitation /> */}
+      {/* <CodeInput /> */}
+      <AuthorizedPage />
     </div>
   )
 }
