8e37ca251833b444be2d4539c3bbf8356c3f5606
Nexus <egor2001grashin@gmail.com>
2023-05-14 23:41:56 +0300

    made devConstants

---
+++ src/devConstants.ts
@@ -0,0 +1,7 @@
+export const userAvatar = 'https://cdn.discordapp.com/attachments/1073325522446188575/1101564001743278181/8edae30f7e0ec6ec0ecf7f33e23c6ab5.png'
+export const inviterAvatar = 'https://cdn.discordapp.com/avatars/332870835310428170/18a97685e370fc85843781413fbf86a6.png?size=4096'
+
+export const currentName = 'Nexus'
+export const inviterName = 'DepoSit'
+
+export const randomAvatar = 'https://cdn.discordapp.com/attachments/1101177259643125820/1101178601753280522/fy4bd2jQfPI.png'
