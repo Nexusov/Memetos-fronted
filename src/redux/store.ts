7f5a4072b0db17b79de55d65dd83c17d9c684ca8
Nexus <egor2001grashin@gmail.com>
2023-05-15 00:52:26 +0300

    made redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import { authSlice } from '../services/auth'
+import { discordApi } from '../services/discord'
+import { gameSlice } from '../services/game'
+import { gameApi } from '../services/gameApi'
+
+export const store = configureStore({
+  reducer: {
+    [authSlice.name]: authSlice.reducer,
+    [gameSlice.name]: gameSlice.reducer,
+    [gameApi.reducerPath]: gameApi.reducer,
+    [discordApi.reducerPath]: discordApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(
+        discordApi.middleware,
+        gameApi.middleware
+      )
+})
+
+export type RootStore = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
