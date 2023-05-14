1b68bea92c5df5da420092fc5e6745aea3a8cbc0
Nexus <egor2001grashin@gmail.com>
2023-05-15 00:52:59 +0300

    made navigations in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,71 @@
-import Home from './pages/Home'
-import Invitation from './pages/Invitation'
-import CodeInput from './pages/CodeInput'
-
 import style from './App.module.scss'
-import AuthorizedPage from './pages/AuthorizedPage'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div className={style.container}>
+        <Outlet />
+      </div>
+    ),
+    children: [
+      {
+        index: true,
+        lazy: async () => {
+          const { Home } = await import('./pages/Home/Home')
+          return { Component: Home }
+        }
+      },
+      {
+        path: 'invite',
+        lazy: async () => {
+          const { CodeInput } = await import('./pages/CodeInput/CodeInput')
+          return { Component: CodeInput }
+        }
+      },
+      {
+        path: 'invite/:code',
+        lazy: async () => {
+          const { Invitation } = await import('./pages/Invitation/Invitation')
+          return { Component: Invitation }
+        }
+      },
+      {
+        path: 'game',
+        lazy: async () => {
+          const { Game } = await import('./pages/Game/Game')
+          return { Component: Game }
+        }
+      },
+      {
+        path: '*',
+        lazy: async () => {
+          const { NotFoundPage } = await import('./pages/404/404')
+          return { Component: NotFoundPage }
+        }
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'discord',
+        lazy: async () => {
+          const { DiscordAuth } = await import('./pages/Auth/DiscordAuth')
+          return { Component: DiscordAuth }
+        }
+      }
+    ]
+  }
+])
 
 const App = () => {
   return (
-    <div className={style.container}>
-      {/* <Home /> */}
-      {/* <Invitation /> */}
-      {/* <CodeInput /> */}
-      <AuthorizedPage />
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
