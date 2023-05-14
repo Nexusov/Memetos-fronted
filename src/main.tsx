0d4ee9f357c1ce20b5be4483c27812edacf3ee2c
Nexus <egor2001grashin@gmail.com>
2023-05-15 00:54:05 +0300

    added store in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import localforage from 'localforage'
+
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 import './normalize.scss'
 import './index.scss'
-
 import App from './App'
+
+await localforage.setDriver(localforage.LOCALSTORAGE)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
