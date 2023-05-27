8896b1f7f9bbc28c6dfc0eac82a30bbdf25400d0
Nexus <egor2001grashin@gmail.com>
2023-05-27 16:25:59 +0300

    RootState in Invitation

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -20,5 +20,5 @@ export const store = configureStore({
       )
 })
 
-export type RootStore = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
