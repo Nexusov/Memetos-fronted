ec1d05c695949dc23cbfde25ce22bec43a3cb116
Nexus <egor2001grashin@gmail.com>
2023-05-27 16:27:38 +0300

    fixed strokeWidth error

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -12,7 +12,7 @@ export const Loader = () => {
       shapeRendering: 'auto'
     }}
       width="98px" height="98px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-      <circle cx="50" cy="50" fill="none" stroke="#e7a3ff" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138">
+      <circle cx="50" cy="50" fill="none" stroke="#e7a3ff" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138">
         <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1.3513513513513513s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
       </circle>
     </svg>
