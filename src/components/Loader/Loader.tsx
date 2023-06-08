bcefbbc0c7d114f81ad583df60663b0f7da84e78
Nexus <egor2001grashin@gmail.com>
2023-06-08 19:04:46 +0300

    fixed Loader transition

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -42,7 +42,7 @@ export const Loader = ({ type }: LoaderProps) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={style}
       width="98px" height="98px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-      <circle cx="50" cy="50" fill="none" stroke="#e7a3ff" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138">
+      <circle cx="50" cy="50" fill="none" stroke="#e7a3ff" strokeWidth="10" r="35" strokeDasharray="164.93361431346415 56.97787143782138" style={{ transition: 'none' }}>
         <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1.3513513513513513s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
       </circle>
     </svg>
