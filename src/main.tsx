8b04f7302b6315cc6a4293816337b68ba0ab553f
Nexus <egor2001grashin@gmail.com>
2023-06-14 22:47:34 +0300

    removed localforage

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import localforage from 'localforage'
 
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
@@ -8,8 +7,6 @@ import { store } from './redux/store'
 import './normalize.scss'
 import './index.scss'
 import App from './App'
-
-await localforage.setDriver(localforage.LOCALSTORAGE)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
